deee230f0bf9aa7c0a0201c540fb41f4b78c5cd4
misita-code <wallace.misita@student.moringaschool.com>
2024-11-28 15:24:06 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
 function scuberGreetingForFeet (feet) {
   if (feet <= 400) {
     return 'This one is on me!';
